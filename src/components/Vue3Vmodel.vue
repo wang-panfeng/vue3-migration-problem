99fb37f7b4a5c5d068e5a6fc4aff41e64f86fffd
wangpanfeng <information_wang@126.com>
2024-12-08 00:51:10 +0800

    feat：增加描述

--- src/components/Vue3Vmodel.vue
+++ src/components/Vue3Vmodel.vue
@@ -10,6 +10,9 @@ function update() {
 <template>
   <div>
     <div>
+      <div>commit id is: f0b18ea2e32f404f4f154080c56d1df346f7c3ea</div>
+    </div>
+    <div>
       v-model of vue3 Syntax. Global compatibility mode is vue2, and disable
       `v-model` compatibility for Vue2， This v-model works well, but vue2
       v-model not work
